fe71f5eeaa6c73909bb049f62376a59d17a20b99
Adrian Gonzalez <rckstr@outlook.com>
2019-09-02 22:43:34 -0700

    added ng2-charts+chart.js+ng2-charts schematics

--- gltrack-app/src/app/app.module.ts
+++ gltrack-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,7 +26,8 @@ import { TablaRegistrosComponent } from './components/tabla-registros/tabla-regi
     AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
-    MaterialModule
+    MaterialModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
